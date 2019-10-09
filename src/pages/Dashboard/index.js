d57d529739cae13e7c6c2ce58543bc8dee0f1bc3
bruno <brunocpessoal@gmail.com>
2019-10-09 17:17:51 +0000

    finished spot watch page

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
+import { Link } from 'react-router-dom';
+
+import './styles.css';
 
 export default function Dashboard() {
   const [spots, setSpots] = useState([]);
@@ -19,14 +22,18 @@ export default function Dashboard() {
 
   return (
     <>
-      <ul className="spotList">
+      <ul className="spot-list">
         {spots.map(spot => (
           <li key={spot._id}>
-            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }}/>
+            <header style={{ backgroundImage: `url(${spot.thumbnail_url})` }} />
             <strong>{spot.company}</strong>
-            <span>{spot.price}</span>
+            <span>{spot.price ? `R${spot.price}/dia`: 'GRATUITO'}</span>
           </li>
         ))}
       </ul>
+
+      <Link to="/new">
+        <button className="btn">Cadastrar novo spot</button>
+      </Link>
     </>);
 };
